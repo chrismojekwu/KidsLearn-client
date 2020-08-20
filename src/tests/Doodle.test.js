ebae880f58dee8c54458ca7947f725f249189a8f
Christopher Mojekwu <chrismojekwu@gmail.com>
2020-08-20 14:09:34 -0500

    added missing tests

--- src/tests/Doodle.test.js
+++ src/tests/Doodle.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Doodle from "../Doodle/Doodle";
+import Canvas from "../Doodle/Canvas";
 
 it("renders Doodle component without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(Doodle, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders Canvas component without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(Canvas, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -6,6 +6,9 @@ import Login from "../components/Login";
 import Progress from "../components/Progress";
 import ProgressForm from "../components/ProgressForm";
 import Signup from "../components/Signup";
+import ReportSharePage from "../components/ReportSharePage";
+import WindowSize from "../components/WindowSize";
+import DoodleColors from "../components/DoodleColors";
 
 it("renders Footer component without crashing", () => {
   const div = document.createElement("div");
@@ -40,5 +43,23 @@ it("renders ProgressForm component without crashing", () => {
 it("renders Signup component without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(Signup, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders Report Share Page component without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(ReportSharePage, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders Window size component without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(WindowSize, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders Doodle Color Bar component without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(DoodleColors, div);
   ReactDOM.unmountComponentAtNode(div);
 });
